8604ac02f4793446750034fd8f8bacb469d34c52
Miles Wu <miles.wu@wu-home.co.uk>
2016-04-03 16:10:02 -0500

    Generate compact peer entry

--- src/announce.rs
+++ src/announce.rs
@@ -1,8 +1,12 @@
 use hyper::server::Request;
 use std::sync::Mutex;
 extern crate redis;
+extern crate byteorder;
 
 use common::*;
+use self::byteorder::{BigEndian, WriteBytesExt};
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
 
 pub fn announce(req: &Request, redis_connection: &Mutex<redis::Connection>) -> Result<Vec<u8>, String>
 {
@@ -26,5 +30,25 @@ pub fn announce(req: &Request, redis_connection: &Mutex<redis::Connection>) -> R
         .and_then(|i| i.parse::<u64>().map_err(|_| "Invalid left specified".to_owned())));
     let ip = req.remote_addr.ip();
 
+    // Generate compact peer entry
+    let ip_v4 = match ip {
+        IpAddr::V4(i) => i,
+        _ => return Err("Ipv6 not implemented yet".to_owned()),
+    };
+    let peer_v4 = generate_peer_ipv4(ip_v4, port);
+
+
     return Ok("worked".to_owned().into_bytes());
+}
+
+fn generate_peer_ipv4(ip: Ipv4Addr, port: u16) -> Vec<u8> {
+    let mut retval = Vec::with_capacity(6);
+    retval.extend(ip.octets().iter());
+    retval.write_u16::<BigEndian>(port).unwrap();
+    return retval;
+}
+
+#[test]
+fn generate_peer_ipv4_test() {
+    assert_eq!(generate_peer_ipv4(Ipv4Addr::new(127, 0, 0, 1), 0x3039), &[127, 0, 0, 1, 0x30, 0x39]);
 }
